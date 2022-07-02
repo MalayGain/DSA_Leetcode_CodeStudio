20e9f6676b1f62b6be7f679a89e9ef6f95a0224a
MalayGain <65612251+MalayGain@users.noreply.github.com>
2022-07-02 10:18:07 +0530

    Create Bottom View of Binary Tree.cpp
    Given a binary tree, print the bottom view from left to right.
    A node is included in bottom view if it can be seen when we look at the tree from bottom.

---
+++ Bottom View of Binary Tree.cpp
@@ -0,0 +1,46 @@
+vector <int> bottomView(Node *root) {
+        // Your Code Here
+  vector<int> bottomView;
+   
+   queue<pair<Node*,int>> q; // (node, line.no)
+   
+   if(root)q.push({root,0});
+   
+   map<int,int> m; // (line.no, node)
+   
+   m[0]=root->data;
+   
+   //marking each node with index of a vertical line while level traversal
+   while(!q.empty())
+   {
+       //leftView.push_back(q.front()->data);
+       
+       int n=q.size();
+       
+       for(int i=1;i<=n;i++)
+       {
+           auto tmp=q.front();
+           q.pop();
+           if(tmp.first->left)
+           {
+               q.push({tmp.first->left,tmp.second-1});
+               m[tmp.second-1]=tmp.first->left->data;
+           }
+           if(tmp.first->right)
+           {
+               q.push({tmp.first->right,tmp.second+1});
+              m[tmp.second+1]=tmp.first->right->data;
+           }
+       }
+   }
+   
+   
+   for(auto e:m)
+   {
+       bottomView.push_back(e.second);
+   }
+   
+   
+   
+    return bottomView;    
+  }
